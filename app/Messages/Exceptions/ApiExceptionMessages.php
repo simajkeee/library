61e2c8f9d3c7c9495d84fa5d31db5b7bee347868
simajkeee <37087698+simajkeee@users.noreply.github.com>
2022-10-02 11:57:00 +0300

    add new exception messages

--- app/Messages/Exceptions/ApiExceptionMessages.php
+++ app/Messages/Exceptions/ApiExceptionMessages.php
@@ -5,7 +5,18 @@ namespace App\Messages\Exceptions;
 
 class ApiExceptionMessages
 {
-    static public function noApiKey(string $className) {
+    static public function noApiKey(string $className): string
+    {
         return sprintf('API key is not provided for %s service', $className);
+    }
+
+    static public function requiredParams(array $params): string
+    {
+        return sprintf("Required parameters are not provided: %s", implode(", ", $params));
+    }
+
+    static public function unableToAccessUrl(string $url): string
+    {
+        return sprintf("Unable to access url: %s", $url);
     }
 }
