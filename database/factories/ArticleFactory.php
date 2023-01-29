1a8600617e59731919e043aa265da062c41c6ffc
simajkeee <37087698+simajkeee@users.noreply.github.com>
2023-01-29 12:20:21 +0300

    set factories relations for user, article and comment models

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->realText(),
+            'content' => fake()->realText(),
+            'author_id' => User::factory(),
+        ];
+    }
+}
---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+
+        return [
+            'content' => fake()->text(),
+            'user_id' => User::factory(),
+            'article_id' => Article::factory(),
+        ];
+    }
+}
