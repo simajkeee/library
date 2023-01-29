777232d7971ad682c97f2d99ca4654a4f514674d
simajkeee <37087698+simajkeee@users.noreply.github.com>
2023-01-29 12:21:42 +0300

    add population of user, articles and comments in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +17,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()
+        ->create([
+            "name"  => "Test User",
+            "email" => "test@example.com",
+        ]);
 
-         \App\Models\User::factory()->create([
-             "name" => "Test User",
-             "email" => "test@example.com",
-             "password" => bcrypt("12345678"),
-         ]);
+        $articles = Article::factory()->count(10)->create([
+            'author_id' => $user->id
+        ]);
+
+
+        $commentUsers = User::factory()->count(10)->create()->toArray();
+        $nestingLvl = 4;
+        $numOfCommentsForEachLvl = 3;
+        foreach ($articles as $article) {
+            $nested = null;
+            for ($i = 0; $i < $nestingLvl; $i++) {
+                $nextComment = Arr::random($commentUsers);
+                if (!$nested) {
+                    $nested = Comment::factory(null, [
+                        'user_id' => $nextComment['id'],
+                        'article_id' => $article->id,
+                    ])->count($numOfCommentsForEachLvl);
+                    continue;
+                }
+                $nested = Comment::factory(null, [
+                    'user_id' => $nextComment['id'],
+                    'article_id' => $article->id,
+                ])->count($numOfCommentsForEachLvl)->has($nested, 'replies');
+            }
+            $nested->create();
+        }
     }
 }
