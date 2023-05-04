e4dc92bd44a061339c64608144c2e30b44c794b6
simajkeee <37087698+simajkeee@users.noreply.github.com>
2023-05-04 12:25:40 +0300

    add password field to db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         ->create([
             "name"  => "Test User",
             "email" => "test@example.com",
+            "password" => bcrypt("123456")
         ]);
 
         $commentUsers = User::factory()->count(10)->create()->toArray();
